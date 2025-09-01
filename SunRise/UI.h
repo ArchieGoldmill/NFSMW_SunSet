40f65330c155aef5adc8d63cd8042ab00c355f48
ARCHIE <ARCHIE@ARCHIE-PC>
2025-09-01 13:03:43 +0200

    Toggle UI

--- SunRise/UI.h
+++ SunRise/UI.h
@@ -7,105 +7,133 @@
 #include "imgui/imgui_impl_win32.h"
 #include "minhook/MinHook.h"
 
-typedef HRESULT(__stdcall* tEndScene)(LPDIRECT3DDEVICE9);
-typedef HRESULT(__stdcall* tReset)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
-
-tEndScene oEndScene;
-tReset oReset;
-WNDPROC oWNDPROC;
-bool initUI = false;
-bool wndProcHooked = false;
-
-HRESULT __stdcall hookedReset(LPDIRECT3DDEVICE9 pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters)
-{
-	if (initUI)
-	{
-		ImGui_ImplDX9_Shutdown();
-		ImGui_ImplWin32_Shutdown();
-		ImGui::DestroyContext();
-
-		initUI = false;
-	}
-
-	return oReset(pDevice, pPresentationParameters);
-}
-
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+
+namespace UI
 {
-	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+	typedef HRESULT(__stdcall* tEndScene)(LPDIRECT3DDEVICE9);
+	typedef HRESULT(__stdcall* tReset)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
+
+	tEndScene oEndScene;
+	tReset oReset;
+	WNDPROC oWNDPROC;
+	bool initUI = false;
+	bool wndProcHooked = false;
+	bool DrawUI = true;
+
+	HRESULT __stdcall hookedReset(LPDIRECT3DDEVICE9 pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters)
 	{
-		return true;
-	}
-
-	return CallWindowProc(oWNDPROC, hWnd, msg, wParam, lParam);
-}
-
-void InitUI(LPDIRECT3DDEVICE9 pDevice)
-{
-	if (!initUI)
-	{
-		IMGUI_CHECKVERSION();
-		ImGui::CreateContext();
-		ImGui::StyleColorsDark();
-
-		if (!wndProcHooked)
+		if (initUI)
 		{
-			oWNDPROC = (WNDPROC)SetWindowLongPtr(Game::Window, GWLP_WNDPROC, (LONG_PTR)WndProc);
-			wndProcHooked = true;
+			ImGui_ImplDX9_Shutdown();
+			ImGui_ImplWin32_Shutdown();
+			ImGui::DestroyContext();
+
+			initUI = false;
 		}
 
-		ImGui_ImplWin32_Init(Game::Window);
-		ImGui_ImplDX9_Init(pDevice);
-
-		initUI = true;
+		return oReset(pDevice, pPresentationParameters);
 	}
-}
 
-HRESULT __stdcall hookedEndScene(LPDIRECT3DDEVICE9 pDevice)
-{
-	InitUI(pDevice);
-
-	ImGui_ImplDX9_NewFrame();
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
-
-	UI::Draw();
-
-	ImGui::EndFrame();
-	ImGui::Render();
-	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
-
-	return oEndScene(pDevice);
-}
-
-void SubInitUI()
-{
-	LPDIRECT3DDEVICE9 pDevice = NULL;
-	while (pDevice == NULL)
+	void HandleKeyDownInput(int wParam)
 	{
-		pDevice = Game::Device;
-		Sleep(1000);
+		if (wParam < 1 || wParam > 0xFF)
+		{
+			return;
+		}
+
+		if (g_Config.HK_ToggleEditor == wParam)
+		{
+			DrawUI = !DrawUI;
+		}
 	}
 
-	auto vt = *(void***)pDevice;
-
-	MH_Initialize();
-
-	MH_CreateHook(vt[16], hookedReset, (LPVOID*)&oReset);
-	MH_CreateHook(vt[42], hookedEndScene, (LPVOID*)&oEndScene);
-
-	MH_EnableHook(MH_ALL_HOOKS);
-}
-
-void InitUI()
-{
-	if (!g_Config.Editor)
+	LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
-		return;
+		if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		{
+			return true;
+		}
+
+		switch (msg)
+		{
+		case WM_KEYDOWN:
+			HandleKeyDownInput((int)wParam);
+			break;
+		}
+
+		return CallWindowProc(oWNDPROC, hWnd, msg, wParam, lParam);
 	}
 
-	injector::MakeNOP(0x006E6CAE, 7);
+	void InitUI(LPDIRECT3DDEVICE9 pDevice)
+	{
+		if (!initUI)
+		{
+			IMGUI_CHECKVERSION();
+			ImGui::CreateContext();
+			ImGui::StyleColorsDark();
 
-	std::thread(SubInitUI).detach();
+			if (!wndProcHooked)
+			{
+				oWNDPROC = (WNDPROC)SetWindowLongPtr(Game::Window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+				wndProcHooked = true;
+			}
+
+			ImGui_ImplWin32_Init(Game::Window);
+			ImGui_ImplDX9_Init(pDevice);
+
+			initUI = true;
+		}
+	}
+
+	HRESULT __stdcall hookedEndScene(LPDIRECT3DDEVICE9 pDevice)
+	{
+		InitUI(pDevice);
+
+		ImGui_ImplDX9_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+		if (DrawUI)
+		{
+			UI::Draw();
+		}
+
+		ImGui::EndFrame();
+		ImGui::Render();
+		ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+
+		return oEndScene(pDevice);
+	}
+
+	void SubInitUI()
+	{
+		LPDIRECT3DDEVICE9 pDevice = NULL;
+		while (pDevice == NULL)
+		{
+			pDevice = Game::Device;
+			Sleep(1000);
+		}
+
+		auto vt = *(void***)pDevice;
+
+		MH_Initialize();
+
+		MH_CreateHook(vt[16], hookedReset, (LPVOID*)&oReset);
+		MH_CreateHook(vt[42], hookedEndScene, (LPVOID*)&oEndScene);
+
+		MH_EnableHook(MH_ALL_HOOKS);
+	}
+
+	void Init()
+	{
+		if (!g_Config.Editor)
+		{
+			return;
+		}
+
+		injector::MakeNOP(0x006E6CAE, 7);
+
+		std::thread(SubInitUI).detach();
+	}
 }
--- SunRise/dllmain.cpp
+++ SunRise/dllmain.cpp
@@ -71,7 +71,7 @@ void Init()
 	InitShaderLoader();
 	InitHooks();
 	InitAmbientShadow();
-	InitUI();
+	UI::Init();
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
